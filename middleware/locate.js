7c7e0c59e577fc582e574d375d3815e54b1ced2a
CreativeMonke <63314157+CreativeMonke@users.noreply.github.com>
2024-04-03 12:13:07 +0300

    Update locate.js

--- middleware/locate.js
+++ middleware/locate.js
@@ -4,15 +4,6 @@ import Location from '../models/Location.js';
 
 const locateMiddleware = async (req, res, next) => {
   try {
-    const location = await Location.findById(req.params.id);
-
-    if (!location) {
-      return res.status(404).json({
-        status: 'failed',
-        message: 'Location not found'
-      });
-    }
-
     // If the location exists, attach it to the request object so
     // it can be accessed in the subsequent controller without having to query again.
     next();
