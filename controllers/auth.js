06d28654aaa0ec93ebf159af33ad5963586774eb
CreativeMonke <63314157+CreativeMonke@users.noreply.github.com>
2024-04-04 19:53:36 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,41 +50,42 @@ export async function Login(req, res) {
 }
 
 export async function Register(req, res) {
-    const { first_name, last_name, email, password } = req.body;
+    const { first_name, last_name, town, email, password } = req.body;
     try {
         const newUser = new User({
             first_name,
             last_name,
+            town,
             email,
             password,
         });
 
-        const existingUser = await User.findOne({ email });
-        if (existingUser)
-            return res.status(400).json({
-                status: "failed",
-                data: [],
-                message: "An account with that email already exists!",
-            });
-        const savedUser = await newUser.save();
-        const { role, ...user_data } = savedUser._doc;
-        console.log(savedUser);
-        res.status(200).json({
-            status: "success",
-            data: [user_data],
-            message:
-                "Thank you for registering with us. Your account has been successfully created.",
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            status: "error",
-            code: 500,
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+        return res.status(400).json({
+            status: "failed",
             data: [],
-            message: "Internal Server Error",
+            message: "An account with that email already exists!",
         });
-    }
-    res.end();
+    const savedUser = await newUser.save();
+    const { role, ...user_data } = savedUser._doc;
+    console.log(savedUser);
+    res.status(200).json({
+        status: "success",
+        data: [user_data],
+        message:
+            "Thank you for registering with us. Your account has been successfully created.",
+    });
+} catch (err) {
+    console.log(err);
+    res.status(500).json({
+        status: "error",
+        code: 500,
+        data: [],
+        message: "Internal Server Error",
+    });
+}
+res.end();
 }
 
 export async function Logout(req, res) {
@@ -94,22 +95,21 @@ export async function Logout(req, res) {
             return res.sendStatus(204);
         const cookie = authHeader.split("=")[1];
         const accessToken = cookie.split(";")[0];
-        const checkIfBlacklist = await Blacklist.findOne({token : accessToken});
-        
-        if(checkIfBlacklist)
-        return res.sendStatus(204);
-        
+        const checkIfBlacklist = await Blacklist.findOne({ token: accessToken });
+
+        if (checkIfBlacklist)
+            return res.sendStatus(204);
+
         const newBlacklist = new Blacklist({
-            token : accessToken,
+            token: accessToken,
         });
         await newBlacklist.save();
 
-        res.setHeader("Clear-Site-Data",`"cookies"`);
+        res.setHeader("Clear-Site-Data", `"cookies"`);
         res.status(200).json({
             message: "You are logged out!",
         });
-    } catch(err)
-    {
+    } catch (err) {
         res.status(500).json({
             status: "error",
             message: "Internal Server Error",
@@ -118,16 +118,15 @@ export async function Logout(req, res) {
     res.end();
 }
 
-export async function checkLogin(req, res){
+export async function checkLogin(req, res) {
     try {
         res.status(200).json({
             message: "Go for login",
         });
-    } catch(err)
-    {
+    } catch (err) {
         res.status(500).json({
             status: "error",
-            data: {message: err},
+            data: { message: err },
             message: "Internal Server Error",
         });
     }
